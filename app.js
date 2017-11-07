08d35282145b89080d6cd79577adbdc7217de9a4
Robin Luongo <robin.luongo@gmail.com>
2017-11-07 12:39:13 -0500

    switch

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
 const express = require('express');
 const routes = require('./routes');
 const nunjucks = require('nunjucks');
+const chalk = require('chalk');
 const app = express();
 const PORT  = 3000;
 
-app.use(routes);
+
+nunjucks.configure('views', {
+  noCache: true
+});
+
+const obj = {
+    title: "myTitle",
+    people: [{name: 'Allen'}, {name: 'asdf'}, {name: 'Vanessa'}, {name: 'Max'}, {name: 'Cassio'} ]
+};
+
+nunjucks.render('index.html', obj, (err, res) => {
+  if (err) {
+    throw err;
+  }
+  console.log(res);
+});
+
+app.use('/special', (req, res, next) => {
+  console.log(chalk.green(`you've reached the special area`));
+  next();
+})
+
+app.use(['/:route', '/'], (req, res, next) => {
+  let output = ''
+  if (req.method === 'POST') {
+    output = chalk.red('POST') + ' /' + req.params.route;
+  } else if (req.method === 'GET') {
+    if (req.params.route === undefined) {
+      output = chalk.green('GET') + ' /';
+    } else {
+      output = chalk.green('GET') + ' /' + req.params.route;
+    }
+  } else {
+    output = chalk.blue(req.method) + ' /' + req.params.route;
+  }
+  console.log(output);
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  console.log("got there");
+});
+
+app.use(routes);
--- routes.js
+++ routes.js
@@ -1,38 +1,8 @@
 const express = require('express');
-const chalk = require('chalk');
 const nunjucks = require('nunjucks')
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//   res.send('Working');
-//   console.log(chalk.green(req.method) + ' /');
-// });
 
-router.use('/special', (req, res, next) => {
-  console.log(chalk.green(`you've reached the special area`));
-  next();
-})
 
-router.use(['/:route', '/'], (req, res) => {
-  let output = ''
-  if (req.method === 'POST') {
-    output = chalk.red('POST') + ' /' + req.params.route;
-  } else if (req.method === 'GET') {
-    if (req.params.route === undefined) {
-      output = chalk.green('GET') + ' /';
-    } else {
-      output = chalk.green('GET') + ' /' + req.params.route;
-    }
-  } else {
-    output = chalk.blue(req.method) + ' /' + req.params.route;
-  }
-  console.log(output);
-  const title = "myTitle";
-  const people = [{name: 'Allen'}, {name: 'asdf'}, {name: 'Vanessa'}, {name: 'Max'}, {name: 'Cassio'} ];
-  res.render('index.html')
-  nunjucks.render('index.html', function(err, res) {
-    if (err) throw err;
-  });
-});
 
 module.exports = router;
