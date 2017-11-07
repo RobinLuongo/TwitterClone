90a90808a283fa784392a9b59a436e0d400fdcc7
allenhj <allendotjohnson@gmail.com>
2017-11-07 14:30:32 -0500

    Switch

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
 const nunjucks = require('nunjucks');
 const chalk = require('chalk');
 const app = express();
 const PORT  = 3000;
 
-
-nunjucks.configure('views', {
-  noCache: true
-});
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views',
+                  { noCache: true }); // point nunjucks to the proper directory for templates
 
 const obj = {
-    title: "myTitle",
-    people: [{name: 'Allen'}, {name: 'asdf'}, {name: 'Vanessa'}, {name: 'Max'}, {name: 'Cassio'} ]
+    title: 'myTitle',
+    people: [{name: 'Allen'}, {name: 'Robin'}, {name: 'Vanessa'}, {name: 'Max'}, {name: 'Cassio'} ]
 };
 
-nunjucks.render('index.html', obj, (err, res) => {
-  if (err) {
-    throw err;
-  }
-  console.log(res);
-});
+// nunjucks.render('index.html', obj, (err, res) => {
+//   if (err) {
+//     throw err;
+//   }
+//   // console.log(res);
+// });
 
 app.use('/special', (req, res, next) => {
   console.log(chalk.green(`you've reached the special area`));
   next();
-})
+});
 
 app.use(['/:route', '/'], (req, res, next) => {
-  let output = ''
+  let output = '';
   if (req.method === 'POST') {
     output = chalk.red('POST') + ' /' + req.params.route;
   } else if (req.method === 'GET') {
@@ -49,7 +49,8 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-  console.log("got there");
+  // console.log('got there');
+  res.render('index.html', obj);
 });
 
-app.use(routes);
+// app.use(routes);
--- routes.js
+++ routes.js
@@ -2,7 +2,4 @@ const express = require('express');
 const nunjucks = require('nunjucks')
 const router = express.Router();
 
-
-
-
 module.exports = router;
